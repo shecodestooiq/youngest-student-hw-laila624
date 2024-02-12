b65554dc90a5991d738ca58d2b4593afe3be08c8
laila624 <superherrro624@gmail.com>
2024-02-12 03:22:10 +0300

    hw done

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-function showYoungestStudent(students) { }
-
+var student = [
+    {
+      name: " NUTELLA ",
+    },
+    {
+      name: " GALAXY ",
+    },
+    {
+      name: " MYA ",
+    },
+  ];
+  
+  function showYoungestStudent(a, b ,c) {
+   for (var i=0 ; i< 1 ; i++){
+      student[0].age = a
+      student[1].age = b
+      student[2].age = c
+      student[i].age = Math.min(a,b,c);
+   console.log(student[i].age)
+  
+   }
+   
+  }
+  
+  showYoungestStudent(21,32,43);
+  
 module.exports = showYoungestStudent;
